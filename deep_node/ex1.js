8b469ce6869e0392159815ce0aacba48b0441c58
Myke <mykemitrichev@gmail.com>
2020-04-07 15:11:31 +0300

    Added usage of environment variables to declare the base_path

--- deep_node/ex1.js
+++ deep_node/ex1.js
@@ -1,16 +1,27 @@
 "use strict";
 const path = require("path");
+const getStdin = require("get-stdin");
 const fs = require("fs");
 const args = require("minimist")(process.argv.slice(2), {
-  boolean: ["help"],
+  boolean: ["help", "in"],
   string: ["file"],
 });
 
+var BASE_PATH = process.env.BASE_PATH || __dirname;
+
 if (args.help) {
   printHelp();
+} else if (args.in || args._.includes("-")) {
+  getStdin().then(processFile(contents.toString())).catch(error);
 } else if (args.file) {
-  let filePath = path.resolve(args.file);
-  processFile(filePath);
+  let filePath = path.join(BASE_PATH, args.file);
+  fs.readFile(filePath, function (err, contents) {
+    if (err) {
+      error(err.toString());
+    }
+    contents = contents.toString().toUpperCase();
+    process.stdout.write(contents);
+  });
 } else {
   error("Incorect Usage", true);
 }
@@ -32,11 +43,10 @@ function printHelp() {
   console.log("");
   console.log("--help                   print this help");
   console.log("--file={FILENAME}        process the file");
+  console.log("--in, -                  process stdin file");
   console.log("");
 }
 
-function processFile(filePath) {
-  let contents = fs.readFileSync(filePath, "utf8");
-  process.stdout.write(contents);
-  //console.log(contents);
+function processFile(contents) {
+  console.log(contents.toUpperCase());
 }
