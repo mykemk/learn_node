31fdfb6231563437cf8840dce56a56f154853d98
Myke <mykemitrichev@gmail.com>
2020-04-07 12:11:28 +0300

    Added a helper function in the CLI

--- deep_node/ex1.js
+++ deep_node/ex1.js
@@ -1,3 +1,17 @@
 "use strict";
-var args = require("minimist")(process.argv.slice(2));
-console.log(args.hello);
+var args = require("minimist")(process.argv.slice(2), {
+  boolean: ["help"],
+  string: ["file"],
+});
+
+//console.log(args.hello);
+
+printHelp();
+
+function printHelp() {
+  console.log("ex1 usage");
+  console.log("  ex1.js --help");
+  console.log("");
+  console.log("--help         print this help");
+  console.log("");
+}
